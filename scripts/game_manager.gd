f4cf0f27c46d398dc33f17ce9ee9f2796a61833d
Ray1212144 <1349123580@qq.com>
2023-11-25 23:33:05 -0500

    save

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,25 +1,18 @@
 extends Node
 
 
-var players = {}
-@export var score: int = 0
-@export var score_label: Label
+@export var player_1_viewport : Viewport
+@export var player_2_viewport : Viewport
 
-
-func _ready():
-	on_score_update()
-
-
-func on_enemy_death():
-	score += 1
-	on_score_update()
-
-
-func on_score_update():
-	pass
 
 #	score_label.text = "SCORE: %s" % score
+func _ready():
+	player_1_viewport = player_2_viewport
+
 
 func _process(_delta):
 	if Input.is_key_pressed(KEY_BACKSPACE):
 		get_tree().reload_current_scene()
+
+func on_enemy_death():
+	pass
