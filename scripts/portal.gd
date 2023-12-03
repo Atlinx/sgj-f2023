be7976eb6d96244fce3326ca4dba642d31956117
Ray1212144 <1349123580@qq.com>
2023-12-03 18:02:36 -0500

    portal

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -1,22 +1,26 @@
 extends Node2D
 
-var transfer : bool = false
+#var transfer : bool = false
+#
+#func _on_portal_1_body_entered(body):
+#	print(body)
+#	if transfer:
+#		body.global_position = get_tree().get_first_node_in_group("portal2").global_position
+#		transfer = false
 
-func _on_portal_1_body_entered(body):
+#func _on_portal_2_body_entered(body):
+#	print(body)
+#	if transfer:
+#		body.global_position = get_tree().get_first_node_in_group("portal1").global_position
+#		transfer = false
+#
+#func _process(delta):
+#	if Input.is_action_just_pressed("portal"):
+#		transfer = true
+
+
+
+
+
+func _on_body_entered(body):
 	print(body)
-	if transfer:
-		body.global_position = get_tree().get_first_node_in_group("portal2").global_position
-		transfer = false
-
-func _on_portal_2_body_entered(body):
-	print(body)
-	if transfer:
-		body.global_position = get_tree().get_first_node_in_group("portal1").global_position
-		transfer = false
-
-func _process(delta):
-	if Input.is_action_just_pressed("portal"):
-		transfer = true
-
-
-
