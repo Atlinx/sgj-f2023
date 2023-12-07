4f1cc2a79bbcd262d4ee08f363b343c87b88f34e
Ray1212144 <1349123580@qq.com>
2023-12-07 16:59:26 -0500

    save

--- scripts/SplitScreen.gd
+++ scripts/SplitScreen.gd
@@ -44,6 +44,7 @@ func _on_player_2_revive():
 	players["2"].camera.enabled = true
 
 func _on_win():
+	await get_tree().create_timer(5).timeout
 	var current_scene_file = get_tree().current_scene.scene_file_path
 	var next_level_number = current_scene_file.to_int()+1
 	var next_level_path = "res://scenes/level/split_" + str(next_level_number) + ".tscn"
