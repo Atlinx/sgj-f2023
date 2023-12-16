2eeb9eb37824ba826c8faa3cb6cb1e6827665321
Ray1212144 <1349123580@qq.com>
2023-12-15 22:58:30 -0500

    break wall

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -5,43 +5,49 @@ extends CharacterBody2D
 var on_wall : bool = false  # 用于跟踪是否与墙壁碰撞
 var collision_timer : float = 0  # 碰撞计时器
 var collision_threshold : float = 2.0  # 碰撞时间阈值
+var tile_area
 
 func _ready():
 	ai.speed = debug_speed
 
+# 你的代码...
 
+# 修改 _on_area_2d_area_entered 函数
+func _on_area_2d_area_entered(area):
+	tile_area = area
+	if area.is_in_group("highland"):
+		on_wall = true  # 将 on_wall 设置为 true，表示进入了墙壁区域
+		# 执行你希望在进入区域时进行的逻辑
 
-# ... 其他代码 ...
+# 修改 _on_area_2d_area_exited 函数
+func _on_area_2d_area_exited(area):
+	if area.is_in_group("highland"):
+		on_wall = false  # 将 on_wall 设置为 false，表示离开了墙壁区域
+		collision_timer = 0  # 重置碰撞计时器
 
-func _physics_process(_delta):
-	# ... 其他逻辑 ...
+# 修改 _process 函数
+func _process(delta):
+	if on_wall:
+		collision_timer += delta  # 累加碰撞计时器
 
-	# 碰撞检测
-	var collision = move_and_collide(Vector2.ZERO, true, true)
-	
-	if collision != null:
-		var collider = collision.get_collider()
-		if  collider.is_in_group("Wall"):
-			print(2)
-			on_wall = true
-			collision_timer += _delta
-		else:
-			on_wall = false
-			collision_timer = 0
-
-	# ... 其他逻辑 ...
-
-	# 如果在墙壁上摩擦并且碰撞时间超过阈值，将墙壁摧毁
-	if on_wall and collision_timer > collision_threshold:
-		print(0)
-		var tile_map = get_tree().get_first_node_in_group("tilemap")
-		var my_position = global_position
-		var cell_coords = tile_map.local_to_map(tile_map.to_local(my_position))
-		var cell_data = tile_map.get_cell_tile_data(0, cell_coords)
-		if cell_data != null:
-			print(1)
-#			if cell_data.terrain == 1:
+		if collision_timer >= collision_threshold:
+			on_wall = false  # 重置碰撞标志
+			collision_timer = 0  # 重置碰撞计时器
+			var tile_map = get_tree().get_first_node_in_group("tilemap")
+			var tile_position = tile_area.global_position
+			var cell_coords = tile_map.local_to_map(tile_map.to_local(tile_position))
+			
 			# 设置当前单元
-			tile_map.set_cell(0, cell_coords, 0, Vector2i(0, 0), 0)
+			tile_map.set_cell(0, cell_coords, 0, Vector2i(12, 1), 0)
+			
+			# 设置周围八格
+			for x in range(cell_coords.x - 1, cell_coords.x + 2):
+				for y in range(cell_coords.y - 1, cell_coords.y + 2):
+					var current_cell = Vector2i(x, y)
+					var cell_data = tile_map.get_cell_tile_data(0, current_cell)
+					if cell_data != null:
+						# 设置周围八格的单元
+						tile_map.set_cell(0, current_cell, 0, Vector2i(12, 1), 0)
+						tile_map.set_cell(1, current_cell, 0, Vector2i(0,12), 0)
 
 
